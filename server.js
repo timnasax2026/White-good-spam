cafee1ca52caf17480db483323e2d450b5a1007f
timnasax2026 <timnasax@gmail.com>
2026-04-21 19:06:13 +0300

    Update server.js

--- server.js
+++ server.js
@@ -1,15 +1,4 @@
 const express = require('express');
-const path = require('path');
 const app = express();
-const PORT = process.env.PORT || 3000;
-
-// Inasoma mafaili yaliyopo kwenye folder
 app.use(express.static(__dirname));
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'index.html'));
-});
-
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
-});
+app.listen(process.env.PORT || 3000);
